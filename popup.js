a8e88d03dafb97364efdb122bbe66bfdea3da849
jkuan1 <justinkuan2000@gmail.com>
2020-01-11 15:33:08 -0800

    deleted useless stuff from js

--- popup.js
+++ popup.js
@@ -7,19 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
         
         location.href = 'http://www.google.com';
-      chrome.tabs.getSelected(null, function(tab) {
-        d = document;
-  
-        var f = d.createElement('form');
-        f.action = 'http://gtmetrix.com/analyze.html?bm';
-        f.method = 'post';
-        var i = d.createElement('input');
-        i.type = 'hidden';
-        i.name = 'url';
-        i.value = tab.url;
-        f.appendChild(i);
-        d.body.appendChild(f);
-        f.submit();
-      });
+    
     }, false);
   }, false);
