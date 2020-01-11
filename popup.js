cc8e96e89c6a57e97125c2c72ae19fa25b8fcf98
jkuan1 <justinkuan2000@gmail.com>
2020-01-11 15:32:01 -0800

    this is for bruno <3

--- popup.js
+++ popup.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('checkPage');
     checkPageButton.addEventListener('click', function() {
   
+        chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+            console.log(tabs[0].url);
+        });
+        
         location.href = 'http://www.google.com';
       chrome.tabs.getSelected(null, function(tab) {
         d = document;
