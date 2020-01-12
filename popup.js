ad38b7c5346cc15b7dbaada647da731130582acc
bkozaki <bruno.ozaki1@gmail.com>
2020-01-11 18:03:22 -0800

    create new tab

--- popup.js
+++ popup.js
@@ -2,3 +2,11 @@ chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
     console.log(url);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPage = document.getElementById('checkPage');
+    // onClick's logic below:
+    checkPage.addEventListener('click', function() {
+        chrome.tabs.create({"url": "https://google.com"});
+    });
+});
