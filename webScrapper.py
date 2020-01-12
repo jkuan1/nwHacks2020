26ce29b157d912719bff8584ea0dc1e6b7f05a29
ThienH7 <thien.ho@alumni.ubc.ca>
2020-01-11 16:42:28 -0800

    Cleaned up and add documentation for webscrapper

--- webScrapper.py
+++ webScrapper.py
@@ -1,13 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Webiste url and requsting the html
 URL = "https://www.cbc.ca/news/canada/ottawa/shelley-kettles-skating-1.5401235"
 page = requests.get(URL)
 
+# Setup BeatifulSoup web scrapper
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(class_='story')
 lines = results.find_all("p")
 
+# Recombine html paragraphs into one string
 list_lines = []
 for p in range(len(lines)):
     paragraph = lines[p].get_text()
@@ -15,13 +18,3 @@ for p in range(len(lines)):
     final_article = " ".join(list_lines)
 
 print(final_article)
-
-# for i in range(len(lines)):
-    # print(lines[i])
-
-
-# print(results.prettify())
-
-
-
-# print(results.prettify())
